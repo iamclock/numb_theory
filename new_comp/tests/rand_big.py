e45d3c571c1a095edd75c9d5d2dbf648b9b16ec7
clock <clock@isc.tsu.ru>
2015-04-27 01:17:51 +0600

    random generator changed, 1 test fixed

--- new_comp/tests/rand_big.py
+++ new_comp/tests/rand_big.py
@@ -6,29 +6,21 @@
 import random
 
 
-def dihotom(x, y, m):
-	q = x
-	z = 1
-	while y:
-		if (y&1):
-			z = z*q % m
-		y = y >> 1
-		q = q*q % m
-	return z
-
-
-
-#i = 7
-for i in range (1, 63):
-	string = str(i)+".dat"
+for i in range (1, 65):
+	if(i < 10):
+		string = "00"
+	else:
+		string = "0"
+	string += str(i)+".dat"
 	file_dat = open(string, "wt")
 	string = string[0:-4]
 	string = string+".ans"
 	file_ans = open(string, "wt")
 	
-	rand_val1 = random.getrandbits(i+35)
-	rand_val2 = random.getrandbits(i+12)
-	"""
+	
+	
+	rand_val1 = random.getrandbits(i+250)
+	rand_val2 = random.getrandbits(i+200)
 	rand_module = 0
 	while rand_module == 0:
 		rand_module = random.getrandbits(17)
@@ -38,15 +30,12 @@ for i in range (1, 63):
 	print(hex(rand_val1))
 	print(hex(rand_val2))
 	print(hex(rand_module))
-	"""
-	rand_val1 = rand_val1 + rand_val2
-	file_dat.write(str(hex(rand_val1))+'\n'+str(hex(rand_val2))+'\n'+str(hex(rand_module)))
 	
-	#rand_val1 = rand_val1**rand_val2 % rand_module
-	rand_val1 = dihotom(rand_val1, rand_val2, rand_module)
+	file_dat.write(str(hex(rand_val1))+'\n'+str(hex(rand_val2))+'\n'+str(hex(rand_module)))
+	rand_val1 = pow(rand_val1, rand_val2, rand_module)
 	print(hex(rand_val1))
 	
-	file_ans.write(str(hex(rand_val1))+'\n')
+	file_ans.write('0'+'x'+str(hex(rand_val1))[2:].upper()+'\n')
 	
 	string = string[0:-len(str(i))]
 	file_dat.close()
