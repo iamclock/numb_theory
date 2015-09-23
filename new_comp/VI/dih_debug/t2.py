bd525d025d05b8d8938c9d9fa93d1d0e3eb0d8e3
clock <double7@mail.com>
2015-09-23 16:33:25 +0600

    debugs added

---
+++ new_comp/VI/dih_debug/t2.py
@@ -0,0 +1,37 @@
+#! /usr/bin/python3
+
+
+
+
+import random
+
+
+def dihotom(x, y, m):
+	q = x
+	z = 1
+	if y&1:
+		z = x
+	y >>= 1
+	while y:
+		q = q*q % m
+		print("L6= "+str(hex(q))[:2]+str(hex(q))[2:].upper())
+		if y&1:
+			z = z*q % m
+			print("L5= "+str(hex(z))[:2]+str(hex(z))[2:].upper())
+		y = y >> 1
+		#print("L6= "+str(hex(q))[:2]+str(hex(q))[2:].upper())
+	return z
+
+
+x = 0xE92593C7D74C93B4
+y = 0x1ffffffffffffffffffffff
+#z = dihotom(x,y,y)
+
+z = 0x1C681D5039DB64C1E40B99C * 0x136D49B73FB874345ADC506
+print(hex(z))
+z = z % 0x1ffffffffffffffffffffff
+print(hex(z))
+
+#_z = str(hex(z))
+
+#print(_z[:2]+_z[2:].upper())
