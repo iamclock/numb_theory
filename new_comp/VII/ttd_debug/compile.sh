f119486ab0f2892f2345eb25907ddefe30ac1a9c
clock <double7@mail.com>
2015-09-30 16:21:17 +0600

    python the trial division

---
+++ new_comp/VII/ttd_debug/compile.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+
+
+
+
+if [ $# -eq 0 ]
+	then
+		echo -n "Enter file name by first argument: "
+		#var file_name
+		read file_name
+else
+	file_name=$1
+fi
+~/2015/numb_theory/for_compile/translator.py $file_name".l"
+~/2015/numb_theory/for_compile/ltc $file_name"_.l"
+rm *.s *_.l
