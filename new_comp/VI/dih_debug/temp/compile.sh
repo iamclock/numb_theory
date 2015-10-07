afbe882c414968d70eac509fdf57478ade77065c
clock <double7@mail.com>
2015-10-07 16:16:18 +0600

    adds

---
+++ new_comp/VI/dih_debug/temp/compile.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+# скрипт запускает транслятор со старых символов на новые файл name_.l
+# затем компилирует выходной файл
+# и удаляет промежуточные файлы .s _.l
+
+
+
+
+if [ $# -eq 0 ]
+	then
+		echo -n "Enter file name by first argument: "
+		#var file_name
+		read file_name
+else
+	file_name=$1
+fi
+~/2015/numb_theory/for_compile/translator.py $file_name".l"
+#~/2015/numb_theory/for_compile/ltc $file_name"_.l"
+~/2015/numb_theory/new_comp/VI/dih_debug/ltc $file_name"_.l"
+rm $file_name"_.l" $file_name"_.s"
