35b368163af2ffc9d1e25a80368604c420dd5610
clock <double7@mail.com>
2015-10-24 16:15:29 +0600

    for tasks folder added

---
+++ forTasks/gbsteps.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python3
+
+
+import sys 
+
+
+def b-gSteps():
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+if len(sys.argv) < 3:
+	print("To few arguments.")
+	return
+
+result = 0
+
+#result = b-gSteps()
+
+print("result"+str(result))
+
+
+
+
+
+
+return
