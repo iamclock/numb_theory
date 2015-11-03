599a5ee2be5daab1d538540f0a6db746c416d9a9
clock <double7@mail.com>
2015-11-03 17:37:31 +0600

    pow for LN barely done

---
+++ new_comp/VI/milrab_debug/t2.py
@@ -0,0 +1,37 @@
+#! /usr/bin/python3
+
+
+
+
+import random
+
+
+def dihotom(x, y, m):
+	q = x
+	z = 1
+	if y&1:
+		z = x
+	y >>= 1
+	j = 1
+	while y:
+		print("j= "+str(j))
+		q = q*q % m
+		print("q= "+str(hex(q))[:2]+str(hex(q))[2:].upper())
+		if y&1:
+			z = z*q % m
+			print("z= "+str(hex(z))[:2]+str(hex(z))[2:].upper())
+		y = y >> 1
+		j += 1
+		#print("L6= "+str(hex(q))[:2]+str(hex(q))[2:].upper())
+	return z
+
+
+x = 1183550335
+y = 2147483647 #536870911
+
+
+_z = dihotom(x,y,y)
+
+print("x = "+str(hex(x))[:2]+str(hex(x))[2:].upper())
+print("y = "+str(hex(y))[:2]+str(hex(y))[2:].upper())
+print("z = "+str(hex(_z))[:2]+str(hex(_z))[2:].upper())
