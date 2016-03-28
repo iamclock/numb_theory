2f4af95593e6f4ea21b7272872e78b9614310139
Clock <double7@mail.ru>
2016-03-29 01:25:11 +0600

    something

--- course_work/barret/tests/rand_t.py
+++ course_work/barret/tests/rand_t.py
@@ -14,8 +14,8 @@ for i in range (1, 65):
 	string = string+".ans"
 	file_ans = open(string, "wt")
 	
-	rand_val1 = random.getrandbits(5+i*5)
-	rand_val2 = random.getrandbits(200)
+	rand_val1 = random.getrandbits(5+i*7)
+	rand_val2 = random.getrandbits(5+i*4)
 	
 	file_dat.write('0'+'x'+str(hex(rand_val1))[2:].upper() + '\n' + '0'+'x'+str(hex(rand_val2)+'\n')[2:].upper())
 	file_ans.write('0'+'x'+str(hex(rand_val1 % rand_val2))[2:].upper())
