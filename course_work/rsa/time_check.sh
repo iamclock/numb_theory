0176c7ab0168375036db848b9721645168209d8e
Clock <double7@mail.ru>
2016-06-13 23:52:03 +0600

    added

--- course_work/rsa/time_check.sh
+++ course_work/rsa/time_check.sh
@@ -3,8 +3,10 @@
 
 bits=1024
 echo -n > output.txt
+timedatectl | grep "Local"
 while [ $bits -gt 10 ]; do
 	#echo bits is $bits
-	echo -n > rsa_encr.txt > rsa_decr.txt && echo $bits | ./rsa_ | tee -a output.txt
+	echo -n > rsa_encr.txt > rsa_decr.txt && echo $bits | ./rsa_ # | tee -a output.txt
 	let bits=bits-10
 done
+timedatectl | grep "Local"
